4b437c42233b169ac81ec69b55faf24b6491a7ea
louis8598 <54024319+louis8598@users.noreply.github.com>
2020-03-25 00:44:47 +0800

    anaulsis avg

--- reviwes_anaylsis.py
+++ reviwes_anaylsis.py
@@ -5,4 +5,11 @@ with open('reviews.txt','r') as f:
         data.append(line) #將每一行存進data裡面
         count += 1 #count = count + 1
         if count % 1000 == 0: #count 每1000筆印一次，也就是整除1000餘數是0，%是求餘數
-            print(count) #讀取進度
+            print(len(data)) #讀取進度
+print('檔案讀取完了，總共讀取了', len(data),'筆資料')
+
+sum_len = 0
+for datas in data:
+    sum_len += len(datas) #sum_lene = sum_len +len(datas)
+avg = sum_len / len(data) #平均長度
+print(avg)
